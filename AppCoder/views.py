945f6b3cfaa53de96f59273168b98105bfea61eb
Camilo Gaviño <camyg22@gmail.com>
2023-03-08 01:24:23 -0300

    Tercera

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from AppCoder.models import Curso
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. 
 
 def curso(self):
 
